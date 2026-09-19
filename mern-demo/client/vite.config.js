2c7ddd8a97ce7ae8a4a57eb61ceb199f6b40c20c
dangnhan0210-arch <dangnhan0210@gmail.com>
2026-09-19 20:27:12 +0700

    Complete Lab 03 Docker MERN

--- mern-demo/client/vite.config.js
+++ mern-demo/client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
 
         proxy: {
             "/api": {
-                target: "http://localhost:5000",
+                target: "http://mern-backend:5000",
                 changeOrigin: true
             }
         }
